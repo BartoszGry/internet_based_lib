a0d3312638da094c03e878d813e45f68d06bb25e
BarGry <bartoszgrygierczyk01@gmail.com>
2024-02-09 13:35:07 +0100

    json web tokken added :))

--- src/main/java/com/grygierczyk/configuration/SecurityConfiguration.java
+++ src/main/java/com/grygierczyk/configuration/SecurityConfiguration.java
@@ -1,14 +1,27 @@
 package com.grygierczyk.configuration;
 
+import com.grygierczyk.utlis.RSAKeyProperties;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
@@ -17,6 +30,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private final RSAKeyProperties keyProperties;
+
+    public SecurityConfiguration(RSAKeyProperties keyProperties) {
+        this.keyProperties = keyProperties;
+    }
+
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
@@ -37,11 +57,22 @@ public class SecurityConfiguration {
                         authorize.requestMatchers("/auth/**").permitAll())
                 .authorizeHttpRequests(authorize ->
                         authorize.anyRequest().authenticated())
-                                .httpBasic(withDefaults())
-                                .build();
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(withDefaults()))
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(keyProperties.getPublicKey()).build();
+    }
 
-
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        JWK jwk=new RSAKey.Builder(keyProperties.getPublicKey()).privateKey(keyProperties.getPrivateKey()).build();
+        JWKSource<SecurityContext> jwks=new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
 }
 
---
+++ src/main/java/com/grygierczyk/utlis/RSAKeyProperties.java
@@ -0,0 +1,35 @@
+package com.grygierczyk.utlis;
+
+import org.springframework.stereotype.Component;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Component
+public class RSAKeyProperties {
+    private RSAPrivateKey privateKey;
+    private RSAPublicKey publicKey;
+
+    public RSAKeyProperties() {
+        KeyPair pair=KeyGeneratorUtility.generateRsKey();
+        this.publicKey = (RSAPublicKey) pair.getPublic();
+        this.privateKey = (RSAPrivateKey) pair.getPrivate();
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(RSAPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+
+    public RSAPublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(RSAPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+}
